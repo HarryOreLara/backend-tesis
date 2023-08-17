8573315759d83a912e4fd7e0c9c9543f0b7f077e
Harry Ore Lara <harryorelara@gmail.com>
2023-08-17 16:38:57 -0500

    Controlador de medicina y de usuario terminado

--- src/controllers/medicines/medicineController.js
+++ src/controllers/medicines/medicineController.js
@@ -1,84 +1,92 @@
 const Medicine = require("../../models/medicines/medicine.model");
 
 
-// const getAllMedicine =  async (req, res)=>{
-//     try{
-//         const medicine = await Medicine.find().sort({createdAt: -1});
+const getAllMedicine =  async (req, res)=>{
+    try{
+        const medicine = await Medicine.find().sort({createdAt: -1});
 
-//         return res.json({
-//             ok:true,
-//             medicine
-//         });
-//     }catch(error){
-//         return res.status(404).json({
-//             ok: false,
-//             msg: "No hay medicinas"
-//         });
-//     }
-// };
-
-
-// const getOneMedicine_ById =  async (req, res)=>{
-//     const {nombre, _id} = req.params;
-
-//     try{
-//         const medicine = await Medicine.findById({_id});
-
-//         if (!medicine) {
-//             return res.status(404).json({
-//                 ok: false,
-//                 msg: "Medicina no encontrada"
-//               });
-//         }
-
-//         return res.json({
-//             ok:true,
-//             medicine
-//         });
-//     }catch(error){
-//         return res.status(500).json({
-//             ok: false,
-//             msg: "Error en el servidor"
-//           });
-//     }
-// };
+        return res.json({
+            ok:true,
+            medicine
+        });
+    }catch(error){
+        return res.status(404).json({
+            ok: false,
+            msg: "No hay medicinas"
+        });
+    }
+};
 
 
+const getOneMedicine_ById =  async (req, res)=>{
+    const {id} = req.params;
+    const medicine = await Medicine.findById(id);
 
-// //creator
-// const getLisAllMedicine_byId =  async (req, res)=>{
+    try{
 
-// };
+        if (!medicine) {
+            return res.status(404).json({
+                ok: false,
+                msg: "Medicina no encontrada"
+              });
+        }
+
+        return res.json({
+            ok:true,
+            medicine
+        });
+    }catch(error){
+        return res.status(500).json({
+            ok: false,
+            msg: "Error en el servidor"
+          });
+    }
+};
+
+
+const getLisAllMedicine_byCreator =  async (req, res)=>{
+    const {id} = req.params;
+
+    try {
+        const medicineList = await Medicine.find({creator: id})
+        return res.json({
+            ok: true,
+            medicineList
+        });
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: "Error en el servidor al buscar la medicina por el creador"
+          });
+    }
+
+};
 
 
 const postMedicine =  async (req, res)=>{
-    const { nombre, cantidadMedicamentos,horaInicio, horaIntermedio,horaFin } = req.body;
-    const id = req.uid;
+    const { nombre, cantidadMedicamentos,horaInicio, horaIntermedio,horaFin } = req.body;//clorfenamina
+    const id = req.uid;//id Harry
+    const creator = req.uid;
 
     try {
 
-        let medicine = await Medicine.findOne({ nombre });
+        let medicine = await Medicine.find({ nombre,creator });
 
-        //if (medicine.creator == id && medicine.nombre == nombre) {
-        if (medicine) {
+        if (medicine.length === 0) {
+            const newMedicine = new Medicine({nombre, cantidadMedicamentos,horaInicio, horaIntermedio,horaFin, creator:id})
+            await newMedicine.save();
+    
+            res.status(200).json({
+                ok:true,
+                msg:"Medicina guardada",
+                newMedicine
+            });
+        }else{
             return res.status(501).json({
                 ok:false,
-                medicina: medicine,
-                elquecrea: creador,
-                id_token: id
+                msg:"Ya registrada"
             });
         }
-
-
-        const newMedicine = new Medicine({nombre, cantidadMedicamentos,horaInicio, horaIntermedio,horaFin, creator:id})
-        // await newMedicine.save();
-
-        res.status(200).json({
-            ok:true,
-            msg:"Medicina guardada",
-            newMedicine
-        });
-
     } catch (error) {
         res.json({
             ok: false,
@@ -87,17 +95,54 @@ const postMedicine =  async (req, res)=>{
     }
 };
 
-// const updateMedicine =  async (req, res)=>{
 
-// };
+const updateMedicine =  async (req, res)=>{
+    const {id} = req.params;
+    const { nombre, cantidadMedicamentos,horaInicio, horaIntermedio,horaFin } = req.body;//clorfenamina
+
+    try {
+        const medicine = await Medicine.findByIdAndUpdate(id, {nombre, cantidadMedicamentos,horaInicio, horaIntermedio,horaFin}, {new:true});
+        return res.json({
+            ok: true,
+            msg:"Medicina Actualizada",
+            medicine
+        });
+    } catch (error) {
+        return res.status(404).json({
+            ok: false,
+            msg: "Medicina no actualizada"
+        });
+    }
+};
 
 
-// const deleteMedicine =  async (req, res)=>{
+const deleteMedicine =  async (req, res)=>{
+    const {id} = req.params;
+    try {
+        const medicine = await Medicine.findByIdAndRemove(id);
+        
+        return res.json({
+            ok: true,
+            msg:"Medicina eliminada",
+            rol
+        });
+    } catch (error) {
+        return res.status(404).json({
+            ok: false,
+            msg: "Error al eliminar"
+        });
+    }
 
-// };
+};
 
 
 module.exports = {
 
-    postMedicine, 
+    postMedicine,
+    getOneMedicine_ById,
+    getLisAllMedicine_byCreator,
+    updateMedicine,
+    deleteMedicine,
+    getAllMedicine
+
 }
--- src/routes/medicines/medicines.router.js
+++ src/routes/medicines/medicines.router.js
@@ -1,23 +1,23 @@
 const { Router } = require("express");
 
-const {    getAllMedicine, getOneMedicine_ById, getLisAllMedicine_byId, 
-    postMedicine, updateMedicine, deleteMedicine} = require("../../controllers/medicines/medicineController");
+const {    getLisAllMedicine_byCreator, getOneMedicine_ById, getLisAllMedicine_byId, 
+    postMedicine, updateMedicine, deleteMedicine,getAllMedicine} = require("../../controllers/medicines/medicineController");
 const verifyToken = require("../../middlewares/verifyToken");
 
 
 const medicineRouter = Router();
 
-// medicineRouter.get("/getAll",[], getAllMedicine);
+medicineRouter.get("/getAll",[], getAllMedicine);
 
-// medicineRouter.get("/getOne",[], getOneMedicine_ById);
+medicineRouter.get("/getList/:id",[], getLisAllMedicine_byCreator);
 
-// medicineRouter.get("/getList",[], getLisAllMedicine_byId);
+medicineRouter.get("/getOne/:id",[], getOneMedicine_ById);
 
 medicineRouter.post("/post",[verifyToken], postMedicine);
 
-// medicineRouter.get("/update",[], updateMedicine);
+medicineRouter.put("/update/:id",[], updateMedicine);
 
-// medicineRouter.get("/delete",[], deleteMedicine);
+medicineRouter.delete("/delete/:id",[], deleteMedicine);
 
 
 module.exports = medicineRouter;
